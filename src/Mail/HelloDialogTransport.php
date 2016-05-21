dead37f5cba430deda886498d3c34d9e525bf574
Coen Zimmerman <coen@pixelindustries.com>
2016-05-21 12:00:30 +0200

    finished hellodialog transport

--- src/Mail/HelloDialogTransport.php
+++ src/Mail/HelloDialogTransport.php
@@ -1,17 +1,22 @@
 <?php
 namespace Czim\HelloDialog\Mail;
 
-use Czim\HelloDialog\Contracts\HelloDialogApiInterface;
 use Czim\HelloDialog\Contracts\HelloDialogHandlerInterface;
 use Illuminate\Mail\Transport\Transport;
 use Swift_Mime_Message;
 
 class HelloDialogTransport extends Transport
 {
+
     /**
      * @var HelloDialogHandlerInterface
      */
     protected $handler;
+
+    /**
+     * @var Swift_Mime_Message
+     */
+    protected $message;
 
     /**
      * @param HelloDialogHandlerInterface $handler
@@ -26,13 +31,73 @@ class HelloDialogTransport extends Transport
      */
     public function send(Swift_Mime_Message $message, &$failedRecipients = null)
     {
-        // the message is a string made from a view
-        // we can pass that on automatically as the typical replacement
-        // for the content -- as set in the config
+        $this->message = $message;
 
-        // before send() is called, a config should be set specifically for the
-        // mail implementation, so this class may know how to pass on the message
-        // as data to be sent to HD throught the API instance
+        $subject    = $message->getSubject();
+        $templateId = $this->getTemplateId();
+        $from       = $this->getFrom();
+        $replaces   = $this->buildReplaces();
+
+        $success = 0;
+
+        $to = $message->getTo();
+
+        if ( ! $to) {
+            throw new \InvalidArgumentException('No to-address set for send()');
+        }
+
+        foreach ($to as $toAddress => $toName) {
+            if ($this->handler->transactional($toAddress, $subject, $templateId, $from, $replaces) ) {
+                $success++;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
+     * Returns normalized from array for HelloDialog API
+     * 
+     * @return \string[]
+     */
+    protected function getFrom()
+    {
+        $from = $this->message->getFrom();
+
+        if ($from && count($from)) {
+
+            // first entry is the 'from' (address => name)
+            $from = [
+                'email' => array_keys($from)[0],
+                'name'  => array_values($from)[0]
+            ];
+
+        } else {
+            $from = config('hellodialog.mail.from') ?: config('hellodialog.from');
+        }
+        
+        return $from;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getTemplateId()
+    {
+        return (int) (config('hellodialog.mail.template') ?: config('hellodialog.default_template'));
+    }
+
+    /**
+     * @return array
+     */
+    protected function buildReplaces()
+    {
+        $replaceKeys = config('hellodialog.mail.replaces');
+
+        return [
+            array_get($replaceKeys, 'title', '__TITLE__')     => $this->message->getSubject(),
+            array_get($replaceKeys, 'content', '__CONTENT__') => $this->message->getBody()
+        ];
     }
 
 }
